a42245cbeeaad160101d226622bbaf47065fa91b
bogumil <bogumil_latuszek@o2.pl>
2025-09-05 16:25:23 +0200

    new project initialized

---
+++ Object_oriented_piracy/src/test/java/sampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class sampleTest {
+    @Test
+    public void testAddition(){
+        assertEquals(4, (2+2));
+    }
+}
